9915f2c9b14100f0e25d23ac8f82c660b641e659
Ital023 <contadoitalo911@gmail.com>
2024-10-20 15:08:56 -0300

    feat: City tdd finished

--- src/main/java/com/devsuperior/demo/controllers/CityController.java
+++ src/main/java/com/devsuperior/demo/controllers/CityController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/cities")
@@ -20,6 +22,14 @@ public class CityController {
 
     @Autowired
     private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAllSortedByName() {
+        List<CityDTO> citySortedByName = service.getSortedByName();
+
+        return ResponseEntity.ok(citySortedByName);
+    }
+
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
@@ -31,5 +41,7 @@ public class CityController {
 
         return ResponseEntity.created(uri).body(dto);
     }
+
+
 
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -4,7 +4,11 @@ import com.devsuperior.demo.dto.CityDTO;
 import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CityService {
@@ -12,6 +16,7 @@ public class CityService {
     @Autowired
     private CityRepository repository;
 
+    @Transactional
     public CityDTO insert(CityDTO dto) {
         City entity = new City();
 
@@ -22,10 +27,19 @@ public class CityService {
         return new CityDTO(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<CityDTO> getSortedByName() {
+        List<City> entities = repository.findAll(Sort.by("name"));
+
+        return entities.stream().map(x -> new CityDTO(x)).toList();
+    }
 
     private void dtoToEntity(CityDTO dto, City entity) {
         entity.setName(dto.getName());
     }
 
-
 }
+
+
+
+
