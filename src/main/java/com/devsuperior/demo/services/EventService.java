b612399c68c37f4ad051cdb507245e2c611167af
Ital023 <contadoitalo911@gmail.com>
2024-10-20 23:06:03 -0300

    feat: Event Service Insert

---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
+import com.devsuperior.demo.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional(readOnly = true)
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+
+        dtoToEntity(dto, entity);
+
+        entity = repository.save(entity);
+
+        return new EventDTO(entity);
+    }
+
+    private void dtoToEntity(EventDTO dto, Event entity) {
+        entity.setName(dto.getName());
+        entity.setUrl(dto.getUrl());
+        entity.setDate(dto.getDate());
+        entity.setCity(new City(dto.getCityId(), null));
+    }
+
+}
