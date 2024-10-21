4d220d0ae56c1cd248df056392bf423da6eabe08
Ital023 <contadoitalo911@gmail.com>
2024-10-20 23:12:45 -0300

    feat: Event Controller

--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -5,6 +5,8 @@ import com.devsuperior.demo.entities.City;
 import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,12 @@ public class EventService {
     private EventRepository repository;
 
     @Transactional(readOnly = true)
+    public Page<EventDTO> findAllPaged(Pageable pageable) {
+        Page<Event> allEntityPaged = repository.findAll(pageable);
+        return allEntityPaged.map(x -> new EventDTO(x));
+    }
+
+    @Transactional
     public EventDTO insert(EventDTO dto) {
         Event entity = new Event();
 
@@ -25,11 +33,15 @@ public class EventService {
         return new EventDTO(entity);
     }
 
+
+
     private void dtoToEntity(EventDTO dto, Event entity) {
         entity.setName(dto.getName());
         entity.setUrl(dto.getUrl());
         entity.setDate(dto.getDate());
         entity.setCity(new City(dto.getCityId(), null));
     }
+
+
 
 }
